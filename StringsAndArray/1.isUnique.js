4521e0e3f0127f82d3cfb535a7affcacab895267
Peter Park <peterparkk25@gmail.com>
2021-11-25 23:27:14 -0500

    isUnique Algo added

---
+++ StringsAndArray/1.isUnique.js
@@ -0,0 +1,34 @@
+//Implement an algorithm to determine fi a string has all unique characters.
+//What if you cannot use additional data structures?
+
+//Brute force method with O(n^2) time complexity
+const isUnique = function (string) {
+  for (let i = 0; i < string.length - 1; i++) {
+    for (let j = i + 1; j < string.length; j++) {
+      //Every following letter from i is going to be checked
+      if (string[j] === string[i]) return false;
+    }
+  }
+  return true;
+};
+// console.log(isUnique("abcdefa"));
+
+//More efficient way with O(n) time complexity(?)
+const isUnique2 = function (string) {
+  const tempArray = [];
+  for (let i = 0; i < string.length; i++) {
+    let stringChar = string[i];
+    if (tempArray.includes(stringChar)) return false;
+    tempArray.push(stringChar);
+  }
+  return true;
+};
+console.log(isUnique("abcdefa"));
+
+//Using sort method
+const isUnique3 = function (string) {
+  let sortedString = string.sort();
+  console.log(sortedString);
+};
+
+// isUnique3("nblaslknga");
