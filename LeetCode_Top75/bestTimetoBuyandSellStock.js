a5b8d4561826266aabbc70890783c6933d4ebecf
Peter Park <peterparkk25@gmail.com>
2021-11-26 15:55:58 -0500

    CrackingTheCodingInterview folder added, bestTimetoBuyandSellStock added

---
+++ LeetCode_Top75/bestTimetoBuyandSellStock.js
@@ -0,0 +1,22 @@
+//You are given an array "prices" where "prices[i]" is the price of a given stock on the ith day
+//You want to maximize your profit by choosing a single day to buy one stock and
+//choosing a different day in the future to sell that stock.
+//Return the maximum profit you can achieve from this transaction. If you cannot
+//achieve any profit, return 0.
+
+//Example 1:
+//Input: prices = [7,1,5,3,6,4]
+//Output: 5
+//Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 6 - 1 = 5
+//Note that buying on day 2 and selling on day 1 is not allowed. You must buy before you sell
+
+const maximumProfit = function (prices) {
+  let minPrice = NUMBER.MAX_VALUE;
+  let maxProfit = 0;
+  for (let i = 0; i < prices.length; i++) {
+    let price = prices[i];
+    if (price < minPrice) minPrice = price;
+    else if (price - minPrice > maxProfit) maxProfit = price - minPrice;
+  }
+  return maxProfit;
+};
